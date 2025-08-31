bb9bca1832e5b670347791a523780ab21545590e
mlabib2 <mahirlive2000@gmail.com>
2025-08-31 18:17:33 +0800

    (add): time to calcualte

--- Technical_Projects/Password_Encryption/main.py
+++ Technical_Projects/Password_Encryption/main.py
@@ -1,10 +1,25 @@
 import bcrypt
-
 #must be byte string
-password = b"SecretPassword55"
-hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+# hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 
-if bcrypt.checkpw(password, hashed):
-    print("It matches")
-else:
-    print("Didn't Match")
+# username = request.form.get("username")
+# password = request.form.get("password").encode("utf-8") 
+
+#Look user up in BD using username 
+
+# if bcrypt.checkpw(password, hashed):
+#     print("It matches")
+    # return redirect(url_for("user_profile"))
+# else:
+#     print("Didn't Match")
+    # flash("Invalid Credentials")
+
+password = b"SecretPassword55"
+import time 
+
+start = time.time()
+hashed = bcrypt.hashpw(password, bcrypt.gensalt(rounds = 17))
+end = time.time()
+f = end - start 
+print(f)
+
